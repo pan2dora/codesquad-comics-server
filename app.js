cd73bc6827d0752b4348bddc0a0dc23d21145b78
Pandora <pan2dora.brown@gmail.com>
2025-06-08 19:20:35 -0400

    Fixed

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 require("dotenv").config(); //summon dotenv lib
 require("./config/connection.js"); //use connect to db
 require("./config/authStrategy.js"); //auth
-
-// Require the following dependencies: express
 const express = require("express");
+// Require the following dependencies: express
+const session = require("express-session");
+const passport = require("passport");
+
+
 const app = express();
 
-const session = require("express-session")
-const passport = require("passport")
 // Require the following dependencies: morgan, helmet and cors
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const methodOverride = require("method-override");
+
 const bookRoutes = require("./routes/bookRoutes.js");
 const authRoutes = require("./routes/authRoutes");
 
@@ -23,9 +24,8 @@ const authRoutes = require("./routes/authRoutes");
 const PORT = process.env.PORT || 8080;
 
 // Use these packages as a middleware for this project - helmet, morgan (combined or dev), cors
-app.use(methodOverride("_method"));
-app.use(helmet());
 
+app.use(helmet());
 
 app.use(morgan("combined"));
 app.use(cors({ credentials: true, origin: true }));
@@ -42,6 +42,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/books", bookRoutes);
 app.use("/api", authRoutes);
 // Require the following module after the dependencies: path
+
+
+app.use(
+  session({
+    resave:false,
+    saveUninitialized:false,
+    secret: process.env.SECRECT_KEY,
+
+    cookie:{
+      httpOnly: true,
+      secure: false, 
+      maxAge: 1000 * 60 * 60 *24,
+    }
+
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
 
 //Error handling middleware
 app.use((err, req, res, next) => {
