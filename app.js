b94570fee6292b024714b04b1fd944235fd27a5b
Pandora <pan2dora.brown@gmail.com>
2025-06-08 22:37:48 -0400

    Refractored app.js for auth

--- app.js
+++ app.js
@@ -1,69 +1,56 @@
 require("dotenv").config(); //summon dotenv lib
 require("./config/connection.js"); //use connect to db
 require("./config/authStrategy.js"); //auth
+//middlware
 const express = require("express");
-// Require the following dependencies: express
-const session = require("express-session");
-const passport = require("passport");
-
-
-const app = express();
-
-// Require the following dependencies: morgan, helmet and cors
 const morgan = require("morgan");
+const path = require("node:path");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const bookRoutes = require("./routes/bookRoutes.js");
-const authRoutes = require("./routes/authRoutes");
+// Auth
+const session = require("express-session");
+const passport = require("passport");
 
-// Note: Order of where this goes is important!!
-// Create a const variable called app with the value of express()
-
+const app = express();
 // Create a const variable called PORT with the value of 8080
 const PORT = process.env.PORT || 8080;
+
+//Routing
+const bookRoutes = require("./routes/bookRoutes.js");
+const authRoutes = require("./routes/authRoutes");
 
 // Use these packages as a middleware for this project - helmet, morgan (combined or dev), cors
 
 app.use(helmet());
-
 app.use(morgan("combined"));
 app.use(cors({ credentials: true, origin: true }));
-// app.use(methodOverride("_method"));
-const path = require("node:path");
-// # ADD more middleware to app.js before the routes
 
-// Remember to use the right syntax and options
-app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "/public")));
 
+//content routes
 app.use("/api/books", bookRoutes);
-app.use("/api", authRoutes);
+app.use("/auth", authRoutes);
 // Require the following module after the dependencies: path
-
 
 app.use(
   session({
-    resave:false,
-    saveUninitialized:false,
+    resave: false,
+    saveUninitialized: false,
     secret: process.env.SECRECT_KEY,
 
-    cookie:{
+    cookie: {
       httpOnly: true,
-      secure: false, 
-      maxAge: 1000 * 60 * 60 *24,
-    }
-
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
   })
-)
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Error handling middleware
 app.use((err, req, res, next) => {
@@ -72,6 +59,8 @@ app.use((err, req, res, next) => {
       error: { message: "Already have an account? Try logging in." },
       statusCode: err.status || 400,
     });
+  } else {
+    console.log("Passed error handling middleware");
   }
   return res.status(err.status || 500).json({
     error: { message: err.message || "Internal server error." },
---
+++ config/authStrategy.js
@@ -0,0 +1,5 @@
+const passport = require("passport")
+const bcrypt = require("bcrypt")
+// const LocalStrategy = require("")
+// const User = require("../models/userModel")
+
