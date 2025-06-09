4959789ee25857040d26a2b6e14db91acebd4e05
Pandora <pan2dora.brown@gmail.com>
2025-06-09 01:09:07 -0400

    Finished Auth

--- config/authStrategy.js
+++ config/authStrategy.js
@@ -1,5 +1,70 @@
-const passport = require("passport")
-const bcrypt = require("bcrypt")
-// const LocalStrategy = require("")
-// const User = require("../models/userModel")
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local");
+const GoogleStrategy = require("passport-google-oauth0").Strategy;
+const User = require("../models/userModel");
 
+passport.use(
+  new LocalStrategy(async (usernam, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, {
+          message: "Incorrect user name or passwordx",
+        });
+      }
+      const result = await bcrypt.compare(password, user.password);
+
+      if (!result) {
+        return done(null, false, {
+          message: "Inccorrect username or password",
+        });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    async (accessToken, refreshToken, Profiler, done) => {
+      try {
+        const user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          return done(null, user);
+        } else {
+          const newUser = new User({
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            username: profile.emails(0).value,
+            googleId: profile.id,
+          });
+        }
+        await newUser.save();
+        return done(null, newUser);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,14 +2,22 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 
-
-
 const register = async (req, res, next) => {
   const { firstName, lastName, username, password } = req.body;
   console.log("This works", req.body);
   try {
+    //error handling
+    if (error) {
+      return next(error);
+    } else if (!firstName || !username || !password) {
+      return res.status(400).json({
+        error: { message: "Missing required fields." },
+        statusCode: 400,
+      });
+    }
+    //hasing
     const saltRounds = 10;
-    const hashPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     const newUser = {
       firstName: firstName,
@@ -22,31 +30,60 @@ const register = async (req, res, next) => {
 
     await newUser.save();
 
+    req.login(newUser, (error) => {
+      if (error) {
+        return next(error);
+      }
+    });
+
+    newUser.password = undefined;
 
     return res.status(201).json({
       success: { message: "New User Created!" },
       data: { newUser },
     });
   } catch (error) {
-    return res.status(500).json({
-      error: { message: "Internal Server Error!" },
-    });
+    return next(error);
   }
 };
 
 const login = async (req, res, next) => {
-  try {
-    return res.status(200).json({
-      success: { message: "User Logged in" },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: { message: "User not logged in!" },
-    });
+  if (err) {
+    return next(err);
   }
+
+  const userCopy = { ...req.user._doc };
+  userCopy.password = undefined;
+  console.log(userCopy);
+
+  res.status(200).json({
+    success: { message: "Login successful " },
+    data: { user: userCopy },
+  });
+  // try {
+  //   return res.status(200).json({
+  //     success: { message: "User Logged in" },
+  //   });
+  // } catch (error) {
+  //   return res.status(500).json({
+  //     error: { message: "User not logged in!" },
+  //   });
+  // }
 };
 
 const logout = async (req, res, next) => {
+  if (err) {
+    return next(err);
+  }
+
+  req.session.destroy((err) => {
+    if (err) {
+      return next(err);
+    }
+  });
+
+  res.clearCookie("connect.sid");
+
   console.log("Initializing logout controller logic...");
   console.log("Session Destroyed");
   response.clearCookie("connect.sid");
@@ -54,14 +91,19 @@ const logout = async (req, res, next) => {
     success: { message: "User Logged out!" },
     statusCode: 200,
   });
-  function sessionDestruction(err) {
-    //error handling as a final check and a failsafe
-    if (err) {
-      return next(err);
-    }
-  }
-  sessionDestruction();
-  console.log("Logout function activated. Logging out...");
+
+  return res.status(200).json({
+    success: { message: "User logged out!" },
+    statusCode: 200,
+  });
+  // function sessionDestruction(err) {
+  //   //error handling as a final check and a failsafe
+  //   if (err) {
+  //     return next(err);
+  //   }
+  // }
+  // sessionDestruction();
+  // console.log("Logout function activated. Logging out...");
 };
 
 const localLogin = async (req, res, next) => {
